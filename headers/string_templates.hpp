c8f992fc372c8f1bd05a92badb08afcc382ecd10
dannee_v <dannee_v@MacBook-Victor>
2020-08-26 13:00:34 +0900

    Clean code: removed unused string templates in string_templates.hpp

--- headers/string_templates.hpp
+++ headers/string_templates.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 
+// --------------------------------------------------------------
+
 // 1: INCLUDE_GUARD_NAME
 // 2: INCLUDE_GUARD_NAME
 // 3: CONTENT
@@ -19,13 +21,19 @@ const std::string include_guard_tmpl =
 
 )";
 
+// --------------------------------------------------------------
+
 // 1: META_INCLUDE_PATH
 const std::string meta_header_tmpl = R"(#include "%s")";
+
+// --------------------------------------------------------------
 
 // 1: CLASS TYPE
 const std::string meta_friend_register_tmpl = R"(
     friend auto meta::registerMembers<%s>();
     )";
+
+// --------------------------------------------------------------
 
 // 1: TYPE
 // 2: MEMBER_TEMPLATE
@@ -37,6 +45,8 @@ inline auto meta::registerMembers< %s >()
 %s 
     );
 })";
+
+// --------------------------------------------------------------
 
 // 1: TYPE
 // 2: BASE_TYPE
@@ -52,34 +62,10 @@ inline auto meta::registerMembers< %s >()
     ));
 })";
 
+// --------------------------------------------------------------
+
 // 1: SHORT_MEMBER_NAME
 // 2: MEMBER_NAME
 const std::string meta_register_member_tmpl = R"(meta::member("%s", &%s))";
-
-// 1: INCLUDE_GUARD_NAME
-// 2: INCLUDE_GUARD_NAME
-// 3: INCLUDES
-// 4: SPECIALISATION_TEMPLATE
-const std::string FileTemplate =
-    R"(#ifndef %s
-#define %s
-
-%s
-
-namespace meta 
-{
-
-%s
-
-} // end namespace
-
-#endif
-)";
-
-// 1: INCLUDE_NAME
-const std::string IncludeTemplate = R"(#include "%s")";
-
-// 1: INCLUDE_NAME
-const std::string AngledIncludeTemplate = R"(#include <%s>)";
 
 #endif /* STRING_TEMPLATES_HPP_ */
